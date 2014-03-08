f63b95b2804fa145eeb30c4503215228ad91ea45
Niklas Hofer <niklas+dev@lanpartei.de>
2014-03-08 19:12:52 +0100

    use ghope fork of nbtfile to get Int_Array support
    note: took 2 hours to find that monkeypatch

--- lib/rubycraft/nbt_helper.rb
+++ lib/rubycraft/nbt_helper.rb
@@ -1,18 +1,5 @@
 require 'nbtfile'
 require 'zlib'
-class NBTFile::Private::Tokenizer
-  def initialize(io)
-    @gz = io
-    @state = NBTFile::Private::TopTokenizerState.new
-  end
-end
-
-class NBTFile::Emitter
-  def initialize(stream)
-    @gz = stream
-    @state = NBTFile::Private::TopEmitterState.new
-  end
-end
 
 module RubyCraft
   # Handles converting bytes to/from nbt regions, which are compressesed/decompress
